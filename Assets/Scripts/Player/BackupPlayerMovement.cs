0e9e2d78b3c46ce08ddf936623e9321339f79c2c
isacharlott <imai@smail.uni-koeln.de>
2022-06-01 21:54:37 +0200

    Added a new movement script that works, animation not working yet, camera not working yet, attack not working yet

--- Assets/Scripts/Player/BackupPlayerMovement.cs
+++ Assets/Scripts/Player/BackupPlayerMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class BackupPlayerMovement : MonoBehaviour
 { 
     [SerializeField] private float speed;
     [SerializeField] private float jumpPower;
