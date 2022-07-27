170c37315dc21605a892e430f91c5d8bd05e93dd
Dalekkai <kaischmitt99@icloud.com>
2022-07-27 15:38:50 +0200

    Unlocks shooting in egypt level

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -7,6 +7,7 @@ public class PlayerAttack : MonoBehaviour
     public Transform firePoint;
     public GameObject playerBullet;
     public float Force;
+    public bool canShoot;
 
     private void Update()
     {
@@ -15,10 +16,18 @@ public class PlayerAttack : MonoBehaviour
             Shoot();
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.gameObject.tag == "Shoot"){canShoot = true;}
+    }
     void Shoot()
     {
+        if(canShoot) //Checks if the ability is already learned.
+        {
         GameObject BulletIns = Instantiate
         (playerBullet, firePoint.position, Quaternion.identity);
         BulletIns.GetComponent<Rigidbody2D>().velocity = transform.right * Force;
+        }       
     }
 }
