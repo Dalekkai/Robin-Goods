fc156a9dd328fef06bc7962f2f7c0c9798d20e95
isacharlott <imai@smail.uni-koeln.de>
2022-08-07 21:54:49 +0200

    Added spikes in China Museum Level and added text + typing sound (which is not perfect yet) to the transition scenes

--- Assets/Scripts/Texts/DialogueTransitions.cs
+++ Assets/Scripts/Texts/DialogueTransitions.cs
@@ -11,6 +11,7 @@ public class DialogueTransitions : MonoBehaviour
     public float typingSpeed;
     public GameObject continueButton;
     public GameObject dialogueBox;
+    public GameObject typingSound;
 
     public void Start()
     {
@@ -23,13 +24,16 @@ public class DialogueTransitions : MonoBehaviour
 
         if (textDisplay.text == sentences[index])
         {
+            typingSound.SetActive(false);
             continueButton.SetActive(true);
+           
         }
     }
 
     IEnumerator TextRoutine()
     {
         yield return new WaitForSeconds(1.1f);
+        typingSound.SetActive(true);
 
         foreach (char letter in sentences[index].ToCharArray())
         {
@@ -53,6 +57,7 @@ public class DialogueTransitions : MonoBehaviour
         {
             textDisplay.text = "";
             continueButton.SetActive(false);
+            typingSound.SetActive(false);
             dialogueBox.SetActive(false);
             LevelLoader.LoadNextLevel();
         }
