178fc7eab103ef4df589be7fad77a8325912bed0
Dalekkai <kaischmitt99@icloud.com>
2022-08-18 11:46:48 +0200

    Implemented a basic soldier and added a sword

---
+++ Assets/Scripts/Enemy/Soilder.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soilder : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform firePoint;
+    public float force;
+    public float shotTime;
+
+
+
+
+
+    void Start()
+    {
+        Shoot();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    public void Shoot() //Shoots bullets at the faced direction 
+                        //TODO shoots only to the left side
+    {
+        GameObject BulletIns = Instantiate
+        (bullet, firePoint.position, Quaternion.identity);
+        BulletIns.GetComponent<Rigidbody2D>().velocity = -transform.right *force;
+        Invoke("Shoot", shotTime);
+    }
+}
