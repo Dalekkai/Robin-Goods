7d53d6b921c71ad24ff4b22b7c50f90fc0fb38b9
Dalekkai <kaischmitt99@icloud.com>
2022-08-25 12:33:16 +0200

    Set the throwing logic of the speer

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -16,6 +16,14 @@ public class PlayerAttack : MonoBehaviour
     public Transform sword;
 
     public GameObject speer;
+    
+    public GameObject speerRB;
+
+    public Transform throwpoint;
+
+    public Transform speerPoint;
+
+    bool canThrowAgain = true;
 
     private void Update()
     {
@@ -46,21 +54,35 @@ public class PlayerAttack : MonoBehaviour
         }
     }
 
-    void SpeerAttack() // TODO 
+    void SpeerAttack()
     {
         if(speer == null) return;
-        Debug.Log("Throw");
-        Rigidbody2D rb = speer.GetComponent<Rigidbody2D>();
-        rb.isKinematic = false;
-        rb.AddForce(transform.right * 30, ForceMode2D.Impulse);
-        //speer.hasBeenThrown = true; 
+        if(canThrowAgain)
+        {
+            //Instatiates the Prefab with Rb
+            GameObject SpeerIns = Instantiate
+            (speerRB, throwpoint.position, Quaternion.identity);
+
+            //Prevents from bein scaled to big
+            SpeerIns.transform.localScale = new Vector3(6,0.1f,2);
+
+            //Adds a impulse to the right side of the warrior
+            Vector2 direction = transform.right + new Vector3(0, 0.1f, 0);
+            SpeerIns.GetComponent<Rigidbody2D>().AddForce(direction * Force, ForceMode2D.Impulse);
+           
+            //Lets you throw after 3 seconds again
+            canThrowAgain = false;
+            Invoke("LetThrowAgain", 3); 
+            
+        }
+        
     }
 
     void Shoot()
     {
         GameObject BulletIns = Instantiate
         (playerBullet, firePoint.position, Quaternion.identity);
-        BulletIns.GetComponent<Rigidbody2D>().velocity = transform.right * Force;
+        BulletIns.GetComponent<Rigidbody2D>().AddForce(transform.right * 20, ForceMode2D.Impulse);
     }
 
     void MeleAtack()
@@ -80,5 +102,12 @@ public class PlayerAttack : MonoBehaviour
     void MeleDmgCancle() //Changes the sword tag back to make it non dmg. 
     {
         sword.gameObject.tag ="Untagged";
+    }
+
+    void LetThrowAgain()
+    {
+        //Instatiate Speer to make clear the speere can be thrwon again 
+        canThrowAgain = true;
+        Instantiate(speer, speerPoint);
     }
 }
--- Assets/Scripts/Player/SpeerDestroyer.cs
+++ Assets/Scripts/Player/SpeerDestroyer.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShuarSpeer : MonoBehaviour
+public class SpeerDestroyer : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float force;
-
     void Start()
     {
-
-        Destroy(gameObject, 10f);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
