af7d0da8f41b8a962c2f5c11a4b2c69f565b7df2
Dalekkai <kaischmitt99@icloud.com>
2022-08-25 14:40:28 +0200

    Finished the museums part

--- Assets/Scripts/Ecuador/TargetScript.cs
+++ Assets/Scripts/Ecuador/TargetScript.cs
@@ -19,6 +19,7 @@ public class TargetScript : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.tag == "ThrownSpeer")
         {
+            //Makes the Door float upwards and the target come down if hit by a speer.
             door.gravityScale = -1;
             gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
             gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
